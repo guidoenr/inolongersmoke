63cc9b2a7dd3e7016c9e33af1ad53bbfcb4fee13
guidoenr <guidoenr4@gmail.com>
2024-08-07 18:17:43 -0300

    guido:kali | Wednesday 07 Aug - 18:17

--- extension.js
+++ extension.js
@@ -5,9 +5,7 @@ import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/ex
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-
-// javascript months are 0-indexed. XD fuck JS devs
-const START_DATE = new Date(2024, 5, 20); 
+const START_DATE = new Date(2024, 5, 20);
 
 const calculateDaysSinceStart = () => {
     const today = new Date();
@@ -16,29 +14,43 @@ const calculateDaysSinceStart = () => {
     return diffDays;
 }
 
+const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, _('Days Since Start'));
 
         this._label = new St.Label({
-            text: `no fumo hace ${calculateDaysSinceStart()} dias`,
-            style_class: 'system-status-icon'
-            //y_expand: true,
-            //text_color: 
+            text: `No fumo hace ${calculateDaysSinceStart()} días`,
+            style_class: 'indicator-label'
         });
 
         this.add_child(this._label);
         this._updateCounter();
+        
+        // Connect click event
+        this.connect('button-press-event', this._onClick.bind(this));
     }
 
     _updateCounter() {
-        this._label.text = `no fumo hace ${calculateDaysSinceStart()} dias`;
-        // update it each 24 hours
+        this._label.text = `No fumo hace ${calculateDaysSinceStart()} días`;
+        // Update it every 24 hours
         this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 86400, () => {
-            this._label.text = `no fumo hace ${calculateDaysSinceStart()} dias`;
+            this._label.text = `No fumo hace ${calculateDaysSinceStart()} días`;
             return GLib.SOURCE_CONTINUE;
         });
+    }
+
+    _onClick() {
+        this._label.style = `color: ${getRandomColor()};`;  // Change to a random color on click
     }
 
     destroy() {
